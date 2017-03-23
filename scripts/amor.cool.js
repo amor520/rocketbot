35bcf0da4f61640483726a5d07125a4d7a808ad2
Amor <amor90s.zhang@gmail.com>
2017-03-23 16:33:22 +0800

    bot time

--- scripts/amor.cool.js
+++ scripts/amor.cool.js
@@ -24,6 +24,6 @@ module.exports = function (robot) {
             } else {
                 res.send(JSON.parse(body))
             }
-        })
+        });
     });
 };
---
+++ scripts/time.js
@@ -0,0 +1,15 @@
+/**
+ * 时间
+ */
+let moment = require('moment');
+
+module.exports = function (robot) {
+    /**
+     * 获取当前时间
+     */
+    robot.respond(/(.*)(时间 || time || date || 日期 || 几点)(.*)/, function (res) {
+        let date = moment().format('YYYY-MM-DD HH:mm:ss');
+        res.send("当前时间:" + date);
+    });
+
+}

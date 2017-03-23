89ff0a57814b8039b3f6017136ddcbc0fedb7025
Amor <amor90s.zhang@gmail.com>
2017-03-23 19:48:55 +0800

    rm log

--- scripts/tuling.js
+++ scripts/tuling.js
@@ -3,7 +3,6 @@ var tuling = new TULING({ key: '44674ef4014542909416ad050536c340' });
 module.exports = function (robot) {
     robot.hear(/(.*)/, function (res) {
         var info = res.match[0];
-        console.log(info);
         tuling.send({
             userid: 1,
             info: info,

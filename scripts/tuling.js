0d0014bd247b0cdb6e0b2dfed411e28d2cde1ec4
Amor <amor90s.zhang@gmail.com>
2017-03-23 21:46:16 +0800

    输入的信息没有被任何脚本捕获时，调用图灵机器人

--- scripts/tuling.js
+++ scripts/tuling.js
@@ -1,7 +1,10 @@
+/**
+ * 图灵机器人:输入的信息没有被任何脚本捕获时，调用图灵机器人
+ */
 var TULING = require('tuling');
 var tuling = new TULING({ key: '44674ef4014542909416ad050536c340' });
 module.exports = function (robot) {
-    robot.hear(/(.*)/, function (res) {
+    robot.catchAll(/(.*)/, function (res) {
         var info = res.match[0];
         tuling.send({
             userid: 1,
